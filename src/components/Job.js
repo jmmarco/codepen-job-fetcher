e92f2c441b9e05df7bc5bab4e7fab08bb3623c2b
Juan Marco <jmmarco@gmail.com>
2021-05-02 11:43:21 -0300

    Actually return rendered error and add error details

--- src/components/Job.js
+++ src/components/Job.js
@@ -1,79 +1,85 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import JobDetail from './JobDetail'
-import { fetchJobs } from '../utils/api'
-import Switch from './Switch'
-import Header from './Header'
-import Loading from './Loading'
+import React from "react";
+import ReactDOM from "react-dom";
+import JobDetail from "./JobDetail";
+import { fetchJobs } from "../utils/api";
+import Switch from "./Switch";
+import Header from "./Header";
+import Loading from "./Loading";
 
 export default class Job extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       jobs: null,
       hasError: false,
       loading: true,
       remote: false,
-    }
-    this.toggleRemote = this.toggleRemote.bind(this)
+      errorMessage: "",
+    };
+    this.toggleRemote = this.toggleRemote.bind(this);
   }
 
   componentDidMount() {
     fetchJobs()
-      .then(jobs => {
+      .then((jobs) => {
         this.setState({
           jobs,
-          loading: false
-        })
+          loading: false,
+        });
       })
-      .catch(error => {
+      .catch((error) => {
+        console.log("fired", error);
         this.setState({
           jobs: null,
-          hasError: true
-        })
-      })
+          hasError: true,
+          errorMessage: error.message,
+        });
+      });
   }
 
   toggleRemote() {
-
-    this.setState(state => ({
-      loading: !state.loading
-    }))
+    this.setState((state) => ({
+      loading: !state.loading,
+    }));
 
     setTimeout(() => {
-      this.setState(state => ({
+      this.setState((state) => ({
         remote: !state.remote,
-        loading: !state.loading
-      }))
-    }, 1000)
+        loading: !state.loading,
+      }));
+    }, 1000);
   }
 
   render() {
-    const { jobs, hasError, loading, remote } = this.state
+    const { jobs, hasError, loading, remote, errorMessage } = this.state;
 
+    console.log({ hasError });
 
     if (hasError) {
-      <h1>Something went wrong!</h1>
+      return (
+        <div>
+          <h1 className="error">{`Something went wrong: ${errorMessage}`}</h1>
+        </div>
+      );
     }
 
     return (
       <React.Fragment>
-      <Header remote={remote} />
-      <Switch toggleRemote={this.toggleRemote}/>
-      { loading ?
-        <Loading /> :
-        <main>
-        { remote
-          ? jobs && jobs.filter(j => j.remote).map(j => <JobDetail key={j.hashid} job={j}/>)
-          : jobs && jobs.map(j => <JobDetail key={j.hashid} job={j}/>)
-        }
-        </main>
-      }
-
+        <Header remote={remote} />
+        <Switch toggleRemote={this.toggleRemote} />
+        {loading ? (
+          <Loading />
+        ) : (
+          <main>
+            {remote
+              ? jobs &&
+                jobs
+                  .filter((j) => j.remote)
+                  .map((j) => <JobDetail key={j.hashid} job={j} />)
+              : jobs && jobs.map((j) => <JobDetail key={j.hashid} job={j} />)}
+          </main>
+        )}
       </React.Fragment>
-    )
+    );
   }
 }
-
-
-
